d4d984651a50e7969ab81626fb45dbd858422b3d
overv161 <overv161@171c3e36-22b5-11de-85c7-557234b16fc6>
2009-10-10 13:07:53 +0000

    Added !rcon and !lua commands for owners. Custom groups coming after this.

---
+++ beta/lua/ev_plugins/sv_runlua.lua
@@ -0,0 +1,32 @@
+/*-------------------------------------------------------------------------------------------------------------------------
+	Run Lua on the server
+-------------------------------------------------------------------------------------------------------------------------*/
+
+local PLUGIN = { }
+PLUGIN.Title = "Run Lua"
+PLUGIN.Description = "Execute Lua on the server."
+PLUGIN.Author = "Overv"
+PLUGIN.ChatCommand = "lua"
+PLUGIN.Usage = "<code>"
+
+function PLUGIN:Call( ply, args )
+	if ( ply:EV_IsOwner() ) then
+		local code = table.concat( args, " " )
+		
+		if ( #code > 0 ) then
+			me = ply
+			this = ply:GetEyeTrace().Entity
+			RunString( code )
+			
+			evolve:notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " ran Lua code: ", evolve.colors.red, code )
+			
+			this, me = nil
+		else
+			evolve:notify( ply, evolve.colors.red, "No code specified." )
+		end
+	else
+		evolve:notify( ply, evolve.colors.red, evolve.constants.notallowed )
+	end
+end
+
+evolve:registerPlugin( PLUGIN )
