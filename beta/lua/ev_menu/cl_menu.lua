69ced2240a2017632ec0f0c8ffbc03ca7dd1efba
overv161 <overv161@171c3e36-22b5-11de-85c7-557234b16fc6>
2010-03-30 16:11:10 +0000

    - Added new menu :o

---
+++ beta/lua/ev_menu/cl_menu.lua
@@ -0,0 +1,82 @@
+/*-------------------------------------------------------------------------------------------------------------------------
+	Clientside menu framework
+-------------------------------------------------------------------------------------------------------------------------*/
+
+evolve.MENU = {}
+local MENU = evolve.MENU
+MENU.Tabs = {}
+
+function evolve:RegisterTab( tab )
+	tab.Panel = vgui.Create( "DPanel", MENU.TabContainer )
+	tab.Panel.Tab = tab
+	tab.Panel.Paint = function() surface.SetDrawColor( 171, 171, 171, 255 ) surface.DrawRect( 0, 0, tab.Panel:GetWide(), tab.Panel:GetTall() ) end
+	
+	tab:Initialize( tab.Panel )
+	tab:Update()
+	
+	MENU.TabContainer:AddSheet( tab.Title, tab.Panel, tab.Icon, false, false, tab.Description )
+	table.insert( MENU.Tabs, tab )
+end
+
+function MENU:Initialize()
+	self.Panel = vgui.Create( "DFrame" )
+	self.Panel:SetSize( 250, 450 )
+	self.Panel:SetPos( -self.Panel:GetWide(), ScrH() / 2 - self.Panel:GetTall() / 2 )
+	self.Panel:ShowCloseButton( false )
+	self.Panel:SetDraggable( false )
+	self.Panel:SetTitle( "" )
+	self.Panel.Paint = function() end
+	
+	self.TabContainer = vgui.Create( "DPropertySheet", self.Panel )
+	self.TabContainer:SetPos( 0, 0 )
+	self.TabContainer:SetSize( self.Panel:GetSize() )
+	
+	for _, file in ipairs( file.FindInLua( "ev_menu/tab_*.lua" ) ) do
+		include( "ev_menu/" .. file )
+	end
+	
+	self.Panel:MakePopup()
+	self.Panel:SetKeyboardInputEnabled( false )
+end
+
+function MENU:Show()
+	if ( !LocalPlayer():EV_HasPrivilege( "Menu" ) ) then return end
+	if ( !self.Panel ) then MENU:Initialize() end
+	
+	for _, tab in ipairs( MENU.Tabs ) do
+		tab:Update()
+	end
+	
+	self.Panel:SetVisible( true )
+	self.Panel:SetKeyboardInputEnabled( false )
+	self.Panel:SetMouseInputEnabled( true )
+	
+	input.SetCursorPos( 50 + self.Panel:GetWide() / 2, ScrH() / 2 )
+	
+	timer.Create( "EV_MenuShow", 1/60, 0, function()
+		self.Panel:SetPos( self.Panel:GetPos() + ( 110 - self.Panel:GetPos() ) / 7, ScrH() / 2 - self.Panel:GetTall() / 2 )
+		
+		if ( self.Panel:GetPos() > 50 ) then
+			timer.Destroy( "EV_MenuShow" )
+		end
+	end )
+end
+
+function MENU:Hide()
+	if ( !self.Panel ) then return end
+	
+	self.Panel:SetKeyboardInputEnabled( false )
+	self.Panel:SetMouseInputEnabled( false )
+	
+	timer.Create( "EV_MenuShow", 1/60, 0, function()
+		self.Panel:SetPos( self.Panel:GetPos() - ( self.Panel:GetPos() + self.Panel:GetWide() + 10 ) / 5, ScrH() / 2 - self.Panel:GetTall() / 2 )
+		
+		if ( self.Panel:GetPos() < -self.Panel:GetWide() ) then
+			self.Panel:SetVisible( false )
+			timer.Destroy( "EV_MenuShow" )
+		end
+	end )
+end
+
+concommand.Add( "+ev_menu", function() MENU:Show() end )
+concommand.Add( "-ev_menu", function() MENU:Hide() end )
---
+++ beta/lua/ev_menu/tab_players.lua
@@ -0,0 +1,91 @@
+/*-------------------------------------------------------------------------------------------------------------------------
+	Tab with player commands
+-------------------------------------------------------------------------------------------------------------------------*/
+
+include( "tab_players_controls.lua" )
+
+local TAB = {}
+TAB.Title = "Players"
+TAB.Description = "Manage players on the server."
+TAB.Icon = "gui/silkicons/user"
+TAB.Author = "Overv"
+TAB.Privileges = { "Player menu" }
+
+function TAB:Initialize( pnl )
+	// Create the player list
+	self.PlayerList = vgui.Create( "EvolvePlayerList", pnl )
+	self.PlayerList:SetPos( 0, 0 )
+	self.PlayerList:SetSize( pnl:GetParent():GetWide() - 10, pnl:GetParent():GetTall() - 58 )
+	self.PlayerList:SetMultiple( true )
+	
+	// Create the plugin buttons	
+	self.ButKick = vgui.Create( "EvolveButton", pnl )
+	self.ButKick:SetPos( 0, pnl:GetParent():GetTall() - 53 )
+	self.ButKick:SetSize( 56, 22 )
+	self.ButKick:SetButtonText( "Kick" )
+	self.ButKick.DoClick = function()
+		if ( self.ButPlugins:GetButtonText() == "Players" ) then
+			self.PluginList:Reset()
+		end
+		
+		self.PluginList:OpenPluginMenu( evolve:FindPlugin( "Kick" ) )
+		
+		self.PlayerList:MoveTo( -pnl:GetParent():GetWide(), 0, 0.1 )
+		self.PluginList:MoveTo( 0, 0, 0.1 )
+		self.ButPlugins:SetButtonText( "Players" )
+	end
+	
+	self.ButBan = vgui.Create( "EvolveButton", pnl )
+	self.ButBan:SetPos( self.ButKick:GetWide() + 5, pnl:GetParent():GetTall() - 53 )
+	self.ButBan:SetSize( 64, 22 )
+	self.ButBan:SetButtonText( "Ban" )
+	self.ButBan.DoClick = function()
+		if ( self.ButPlugins:GetButtonText() == "Players" ) then
+			self.PluginList:Reset()
+		end
+		
+		self.PluginList:OpenPluginMenu( evolve:FindPlugin( "Ban" ) )
+		
+		self.PlayerList:MoveTo( -pnl:GetParent():GetWide(), 0, 0.1 )
+		self.PluginList:MoveTo( 0, 0, 0.1 )
+		self.ButPlugins:SetButtonText( "Players" )
+	end
+	
+	self.ButPlugins = vgui.Create( "EvolveButton", pnl )
+	self.ButPlugins:SetPos( self.ButKick:GetWide() + self.ButPlugins:GetWide() + 10, pnl:GetParent():GetTall() - 53 )
+	self.ButPlugins:SetSize( pnl:GetParent():GetWide() - 20 - self.ButKick:GetWide() - self.ButPlugins:GetWide(), 22 )
+	self.ButPlugins:SetButtonText( "Plugins" )
+	self.ButPlugins:SetNotHighlightedColor( 50 )
+	self.ButPlugins:SetHighlightedColor( 90 )
+	self.ButPlugins.DoClick = function()
+		if ( self.ButPlugins:GetButtonText() == "Plugins" ) then
+			self.PlayerList:MoveTo( -pnl:GetParent():GetWide(), 0, 0.1 )
+			self.PluginList:MoveTo( 0, 0, 0.1 )
+			self.ButPlugins:SetButtonText( "Players" )
+		else
+			self.PlayerList:MoveTo( 0, 0, 0.1 )
+			self.PluginList:MoveTo( pnl:GetParent():GetWide(), 0, 0.1 )
+			self.PluginList:Reset()
+			self.ButPlugins:SetButtonText( "Plugins" )
+		end
+	end
+	
+	// Create the plugin list
+	self.PluginList = vgui.Create( "EvolvePluginList", pnl )
+	self.PluginList:SetPos( pnl:GetParent():GetWide(), 0 )
+	self.PluginList:SetSize( pnl:GetParent():GetWide() - 10, pnl:GetParent():GetTall() - 58 )
+	self.PluginList:CreatePluginsPage()
+end
+
+function TAB:Update()
+	self.PlayerList:Populate()
+	
+	if ( self.ButPlugins:GetButtonText() != "Plugins" ) then
+		self.PlayerList:SetPos( 0, 0 )
+		self.PluginList:SetPos( self.Panel:GetParent():GetWide(), 0 )
+		self.PluginList:Reset()
+		self.ButPlugins:SetButtonText( "Plugins" )
+	end
+end
+
+evolve:RegisterTab( TAB )
