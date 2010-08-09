7aaac4e9bcd3c843c660c913b2ce29c8ed7c03be
arvid_divran@hotmail.com <arvid_divran@hotmail.com@171c3e36-22b5-11de-85c7-557234b16fc6>
2010-08-09 15:07:15 +0000

    - Made bring & goto check if the destination has any available space so that the player does not get stuck. If it cannot find any free space, it silently fails and forces the player there anyway. - Teleport now makes sure the player does not get stuck halfway inside the wall. If the player wants to teleport inside a narrow place, it will first try to find free space, and as a worst case scenario it will silently fail and force the player there anyway. - Increased health clamp from 99999 to 2147483647.

--- trunk/beta/lua/ev_plugins/sh_bring.lua
+++ trunk/beta/lua/ev_plugins/sh_bring.lua
@@ -5,10 +5,37 @@
 local PLUGIN = {}
 PLUGIN.Title = "Bring"
 PLUGIN.Description = "Bring a player."
-PLUGIN.Author = "Overv"
+PLUGIN.Author = "Overv & Divran"
 PLUGIN.ChatCommand = "bring"
 PLUGIN.Usage = "[players]"
 PLUGIN.Privileges = { "Bring" }
+
+PLUGIN.Positions = {}
+for i=0,360,45 do table.insert( PLUGIN.Positions, Vector(math.cos(i),math.sin(i),0) ) end -- Around
+table.insert( PLUGIN.Positions, Vector(0,0,1) ) -- Above
+
+function PLUGIN:FindPosition( ply )
+	local size = Vector( 32, 32, 72 )
+	
+	local StartPos = ply:GetPos() + Vector(0,0,size.z/2)
+	
+	for _,v in ipairs( self.Positions ) do
+		local Pos = StartPos + v * size * 1.5
+		
+		local tr = {}
+		tr.start = Pos
+		tr.endpos = Pos
+		tr.mins = size / 2 * -1
+		tr.maxs = size / 2
+		local trace = util.TraceHull( tr )
+		
+		if (!trace.Hit) then
+			return Pos - Vector(0,0,size.z/2)
+		end
+	end
+	
+	return false
+end
 
 function PLUGIN:Call( ply, args )
 	if ( ply:EV_HasPrivilege( "Bring" ) and ply:IsValid() ) then	
@@ -16,7 +43,17 @@ function PLUGIN:Call( ply, args )
 		
 		for i, pl in ipairs( players ) do
 			if ( pl:InVehicle() ) then pl:ExitVehicle() end
-			pl:SetPos( ply:GetPos() + Vector( 0, 0, i * 128 ) )
+			if ( pl:GetMoveType() == MOVETYPE_NOCLIP) then
+				pl:SetPos( ply:GetPos() + ply:GetForward() * 45 )
+			else
+				local Pos = self:FindPosition( ply )
+				if (Pos) then
+					pl:SetPos( Pos )
+				else
+					--evolve:Notify( pl, evolve.colors.red, "No free position was found. You were forcefully teleported in front of the player instead." )
+					pl:SetPos( ply:GetPos() + Vector(0,0,72 * i) )
+				end
+			end
 		end
 		
 		if ( #players > 0 ) then
